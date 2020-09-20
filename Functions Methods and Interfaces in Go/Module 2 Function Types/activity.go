92f3ef239bc25f2d03a4b22292428e6a3d1c938c
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-09-20 22:39:13 +0100

    function return assignment

--- Functions Methods and Interfaces in Go/Module 2 Function Types/activity.go
+++ Functions Methods and Interfaces in Go/Module 2 Function Types/activity.go
@@ -1,14 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
 
 	//prompt user to enter acceleration, velocity and displacement
+	acceleration := getUserInput("enter acceleration: ")
+	velocity := getUserInput("enter verlocity: ")
+	displacement := getUserInput("enter displacement: ")
+
+	fn := genDisplaceFn(acceleration, velocity, displacement)
+
+	//promot user to enter time one
+	t1 := getUserInput("enter time value: ")
+	fmt.Print("Result: ")
+	fmt.Println(fn(t1))
+
+	//promot user to enter time one
+	t2 := getUserInput("enter another time value: ")
+	fmt.Print("Result: ")
+	fmt.Println(fn(t2))
 
 }
 
-func GeDisplaceFn(a float64, v float64, d float64) func(t float64) float64 {
+func genDisplaceFn(a float64, v float64, d float64) func(t float64) float64 {
 
-	//add formula to generate and return a function
+	fn := func(t float64) float64 {
+		//formula
+		s := (a*math.Pow(t, 2))/2 + v*t + d
 
-	// return func(t)
+		return s
+	}
+
+	return fn
+}
+
+func getUserInput(message string) float64 {
+
+	fmt.Printf("%s ", message)
+
+	var val float64
+	_, err := fmt.Scan(&val)
+
+	if err != nil {
+		fmt.Println("value entered must be float62")
+	}
+
+	return val
 }
