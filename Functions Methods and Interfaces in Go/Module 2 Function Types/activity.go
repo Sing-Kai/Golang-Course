435abe46b022f37a52e8000ba6b17d08a6de3eda
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-09-20 10:19:47 +0100

    added files for module 2 assignment

---
+++ Functions Methods and Interfaces in Go/Module 2 Function Types/activity.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+	//prompt user to enter acceleration, velocity and displacement
+
+}
+
+func GeDisplaceFn(a float64, v float64, d float64) func(t float64) float64 {
+
+	//add formula to generate and return a function
+
+	// return func(t)
+}
