e51274fb1625ec8215fae4d17bb78529c83822b8
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-09-22 21:41:04 +0100

    update oop assignment

--- Functions Methods and Interfaces in Go/Module 3 Object Orientation/animal.go
+++ Functions Methods and Interfaces in Go/Module 3 Object Orientation/animal.go
@@ -19,38 +19,59 @@ func (a *animal) SetFields(f string, l string, n string) {
 	a.noise = n
 }
 
-func (a *animal) eat() string {
+func (a *animal) Eat() string {
 	return a.food
 }
 
-func (a *animal) move() string {
+func (a *animal) Move() string {
 	return a.locomotion
 }
 
-func (a *animal) speak() string {
+func (a *animal) Speak() string {
 	return a.noise
 }
 
 func main() {
 
-	cow := new(animal)
-	bird := new(animal)
-	snake := new(animal)
+	cow := animal{food: "grass", locomotion: "walk", noise: "moo"}
+	bird := animal{food: "worms", locomotion: "fly", noise: "peep"}
+	snake := animal{food: "mice", locomotion: "sither", noise: "hsss"}
 
-	cow.SetFields("grass", "walk", "moo")
-	bird.SetFields("worms", "fly", "peep")
-	snake.SetFields("mice", "sither", "hsss")
+	animals := []animal{cow, bird, snake}
+
+	fmt.Println("Enter request. Press ctrl + c to exit")
 
 	for {
 
-		fmt.Println("Enter")
 		fmt.Print("> ")
-
 		a, req := scanUserRequest()
 
 		if validEntry(a, req) {
-			fmt.Printf("%s %s\n", a, req)
+			selectedAnimal := getAnimal(a, animals)
+			processRequest(selectedAnimal, req)
 		}
+	}
+}
+
+func processRequest(a animal, req string) {
+
+	if req == "eat" {
+		fmt.Printf("%s \n", a.Eat())
+	} else if req == "move" {
+		fmt.Printf("%s\n", a.Move())
+	} else if req == "speak" {
+		fmt.Printf("%s\n", a.Speak())
+	}
+}
+
+func getAnimal(a string, animals []animal) animal {
+
+	if a == "cow" {
+		return animals[0]
+	} else if a == "bird" {
+		return animals[1]
+	} else {
+		return animals[2]
 	}
 }
 
@@ -73,13 +94,11 @@ func scanUserRequest() (string, string) {
 
 func validEntry(a string, req string) bool {
 
-	if !validAnimal(a) {
-		fmt.Println("Animal not found! Please enter: cow, bird or snake")
-		return false
-	}
-
-	if !validRequest(req) {
-		fmt.Println("Request Not found! Please enter: eat, move or speak")
+	if !validAnimal(a) || !validRequest(req) {
+		fmt.Println("Invalid entry")
+		fmt.Println("Please enter animal followed by action. Example: cow eat")
+		fmt.Println("Valid animals: cow, bird or snake")
+		fmt.Println("Valid actions: eat, move or speak")
 		return false
 	}
 
